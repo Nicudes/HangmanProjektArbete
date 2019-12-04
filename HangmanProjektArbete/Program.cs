a454f81233c36cfe564fa9695da8c3f6d7f4c41b
Huy Tran <Nicudes@gmail.com>
2019-12-04 14:48:22 +0100

    added hejdå

--- HangmanProjektArbete/Program.cs
+++ HangmanProjektArbete/Program.cs
@@ -10,7 +10,7 @@ namespace HangmanProjektArbete
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hej");
+            Console.WriteLine("hej, hejdå!");
         }
     }
 }
